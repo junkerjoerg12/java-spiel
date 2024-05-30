a901f3b75c07c78c893d54f2108d7256053d3baf
rcustic <jakob.meyer06gmx.de>
2024-05-30 20:38:50 +0200

    nur settings geaddet

---
+++ src/main/java/de/junkerjoerg12/Settings.java
@@ -0,0 +1,16 @@
+package de.junkerjoerg12;
+
+import javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Settings extends JLabel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+}
