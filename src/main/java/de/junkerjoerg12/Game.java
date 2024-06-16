bd35ca5b06da525a68732a6fe661e564244b9419
Junkerjoerg12 <138699069+junkerjoerg12@users.noreply.github.com>
2024-06-16 21:31:46 +0200

    stats aus repo entfernt, wird lokal bei spielstart erstellt, falls es noch nicht existiert

--- src/main/java/de/junkerjoerg12/Game.java
+++ src/main/java/de/junkerjoerg12/Game.java
@@ -26,302 +26,302 @@ import javax.swing.Timer;
 
 public class Game extends JFrame implements ActionListener, KeyListener {
 
-    // keybinds
-    private int keyRight = 68;
-    private int keyLeft = 65;
-    private int keyJump = 32;
-    private int keyConsole = 130;
+  // keybinds
+  private int keyRight = 68;
+  private int keyLeft = 65;
+  private int keyJump = 32;
+  private int keyConsole = 130;
 
-    // auf welchem Monitor das Spiel angezeigt werden soll
-    // nur während entwicklung wichtig
-    private byte monitor = 1;
+  // auf welchem Monitor das Spiel angezeigt werden soll
+  // nur während entwicklung wichtig
+  private byte monitor = 1;
 
-    private MainMenu mainMenu;
-    private Map map;
-    public Console console;
-    private Lvlauswahl lvlauswahl;
-    private Endscreen endscreen;
-    private Pause pause;
+  private MainMenu mainMenu;
+  private Map map;
+  public Console console;
+  private Lvlauswahl lvlauswahl;
+  private Endscreen endscreen;
+  // private Pause pause;
 
-    private final int targetFPS = 60;
+  private final int targetFPS = 60;
 
-    private double delayBetweenFrames; // in Millisekunden
+  private double delayBetweenFrames; // in Millisekunden
 
-    private Timer timer;
-    private TimerForMap timerformap;
-    private Gameloop gameloop;
-    private Timer imageSwitcher;
+  private Timer timer;
+  private TimerForMap timerformap;
+  private Gameloop gameloop;
+  private Timer imageSwitcher;
 
-    // misst die Zeit, die das Spiel Läuft
-    private double upTime;
+  // misst die Zeit, die das Spiel Läuft
+  private double upTime;
 
-    private boolean autostart = false;// ob sich das Spiel gleich startet oder man erst ins Main Menue kommt
+  private boolean autostart = false;// ob sich das Spiel gleich startet oder man erst ins Main Menue kommt
 
-    public boolean buildMode;
+  public boolean buildMode;
 
-    private boolean paused = false;
+  private boolean paused = false;
 
-    // Sachen zum testen von performance
-    Timer timerm;
-    int calls = 0;
-    public int updates = 0;
-    public int draws = 0;
-    long start = 0;
-    long afterUpdate = 0;
-    long fertig;
+  // Sachen zum testen von performance
+  Timer timerm;
+  int calls = 0;
+  public int updates = 0;
+  public int draws = 0;
+  long start = 0;
+  long afterUpdate = 0;
+  long fertig;
 
-    public Game() {
-        delayBetweenFrames = Math.floor(1.0 / targetFPS * 1000);
+  public Game() {
+    delayBetweenFrames = Math.floor(1.0 / targetFPS * 1000);
 
-        timerm = new Timer(1000, this);
-        imageSwitcher = new Timer(700, this);
+    timerm = new Timer(1000, this);
+    imageSwitcher = new Timer(700, this);
 
-        this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        if (Toolkit.getDefaultToolkit().getScreenSize().equals(new Dimension(1920, 1080))) {
-            this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        } else {
-            this.setSize(1920, 1080);
-        }
-        this.setUndecorated(true);
-        this.setLayout(new BorderLayout());
-
-        // öffnet spiel auf gewünschtem Monitor
-        this.setLocation(GraphicsEnvironment
-                .getLocalGraphicsEnvironment()
-                .getScreenDevices()[monitor - 1]
-                .getDefaultConfiguration()
-                .getBounds()
-                .getLocation());
-
-        mainMenu();
-        this.setVisible(true);
-
-        this.addKeyListener(this);
-
-        if (autostart) {
-            levelauswahl();
-        }
+    this.setResizable(false);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    if (Toolkit.getDefaultToolkit().getScreenSize().equals(new Dimension(1920, 1080))) {
+      this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    } else {
+      this.setSize(1920, 1080);
     }
+    this.setUndecorated(true);
+    this.setLayout(new BorderLayout());
 
-    public void mainMenu() {
-        if (map != null) {
-            remove(map);
-            gameloop.pause();
-        }
-        this.mainMenu = new MainMenu(this);
-        this.add(mainMenu, BorderLayout.CENTER);
-        mainMenu.setVisible(true);
-        revalidate();
-        repaint();
-        requestFocus();
+    // öffnet spiel auf gewünschtem Monitor
+    this.setLocation(GraphicsEnvironment
+        .getLocalGraphicsEnvironment()
+        .getScreenDevices()[monitor - 1]
+        .getDefaultConfiguration()
+        .getBounds()
+        .getLocation());
+
+    mainMenu();
+    this.setVisible(true);
+
+    this.addKeyListener(this);
+
+    if (autostart) {
+      levelauswahl();
     }
+  }
 
-    public void addmap(String filepath) {
-        remove(lvlauswahl);
-        map = new Map(this, filepath);
-        map.build();
-        map.setVisible(true);
-        this.add(map, BorderLayout.CENTER);
-        timerformap = new TimerForMap();
-        revalidate();
-        repaint();
-
-        this.requestFocus();
-        gameloop = new Gameloop((long) delayBetweenFrames, this);
-        gameloop.start();
-        imageSwitcher.start();
-        timerm.start();
-
+  public void mainMenu() {
+    if (map != null) {
+      remove(map);
+      gameloop.pause();
     }
+    this.mainMenu = new MainMenu(this);
+    this.add(mainMenu, BorderLayout.CENTER);
+    mainMenu.setVisible(true);
+    revalidate();
+    repaint();
+    requestFocus();
+  }
 
-    public void levelauswahl() {
-        lvlauswahl = new Lvlauswahl(this);
-        remove(mainMenu);
-        this.add(lvlauswahl, BorderLayout.CENTER);
-        revalidate();
-        repaint();
-        this.requestFocus();
+  public void addmap(String filepath) {
+    remove(lvlauswahl);
+    map = new Map(this, filepath);
+    map.build();
+    map.setVisible(true);
+    this.add(map, BorderLayout.CENTER);
+    timerformap = new TimerForMap();
+    revalidate();
+    repaint();
+
+    this.requestFocus();
+    gameloop = new Gameloop((long) delayBetweenFrames, this);
+    gameloop.start();
+    imageSwitcher.start();
+    timerm.start();
+
+  }
+
+  public void levelauswahl() {
+    lvlauswahl = new Lvlauswahl(this);
+    remove(mainMenu);
+    this.add(lvlauswahl, BorderLayout.CENTER);
+    revalidate();
+    repaint();
+    this.requestFocus();
+  }
+
+  public void switchScene(JPanel oldpanel, JPanel newpanel) { // sollte bspw Settings removen und lvlauswahl adden
+    remove(oldpanel);
+    add(newpanel, BorderLayout.CENTER);
+    revalidate();
+    repaint();
+    this.requestFocus();
+  }
+
+  public void pause() {
+    gameloop.pause();
+    new Pause(this);
+
+    revalidate();
+    repaint();
+    paused = !paused;
+  }
+
+  public void stoppause(Map map) {
+    map.setVisible(true);
+    gameloop.go();
+    revalidate();
+    repaint();
+  }
+
+  public void setEndscreen() {
+    endscreen = new Endscreen(this, getcurrentmin(), getcurrents(), getcurrentms());
+    gameloop.pause();
+    remove(map);
+    this.add(endscreen, BorderLayout.CENTER);
+    endscreen.setVisible(true);
+    revalidate();
+    repaint();
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // wird immer wieder vom Timer aufgerufen, ist quasi die Gameloop
+    if (e.getSource() == timer) {
+    } else if (e.getSource() == timerm) {
+      // System.out.println("calls: " + calls);
+      // System.out.println("updates: " + updates);
+      // System.out.println("draws: " + draws);
+      // System.out.println("update Time: " + (afterUpdate - start));
+      // System.out.println("drawTime: " + (fertig - afterUpdate));
+      calls = 0;
+      updates = 0;
+      draws = 0;
+    } else if (e.getSource() == imageSwitcher) {
+      // switch image methode von jeder Mapobjekt klasse aufrufen
+      Water.switchImage();
+      Goal.switchImages();
     }
+  }
 
-    public void switchScene(JPanel oldpanel, JPanel newpanel) { // sollte bspw Settings removen und lvlauswahl adden
-        remove(oldpanel);
-        add(newpanel, BorderLayout.CENTER);
-        revalidate();
-        repaint();
-        this.requestFocus();
+  public void tick() {
+    calls++;
+    upTime += delayBetweenFrames;
+    start = System.currentTimeMillis();
+    map.update();
+    afterUpdate = System.currentTimeMillis();
+    map.draw();
+    fertig = System.currentTimeMillis();
+  }
+
+  public long getcurrents() {
+    return timerformap.calculateCurrentTimeInS();
+  }
+
+  public long getcurrentms() {
+    return timerformap.calculateCurrentTimeInMs();
+  }
+
+  public long getcurrentmin() {
+    return timerformap.calculateCurrentTimeInMin();
+  }
+
+  public boolean alreadybound(int key) {
+    if (key == keyJump) {
+      return true;
+    } else if (key == keyRight) {
+      return true;
+    } else if (key == keyLeft) {
+      return true;
+    } else if (key == keyConsole) {
+      return true;
     }
+    return false;
+  }
 
-    public void pause() {
-        gameloop.pause();
-        pause = new Pause(this);
+  @Override
+  public void keyTyped(KeyEvent e) {
+  }
 
-        revalidate();
-        repaint();
-        paused = !paused;
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == keyRight) {
+      if (!map.getPlayer().collisionRight(map.getAllObjects())) {
+        map.getPlayer().walkRight = true;
+      }
+    } else if (e.getKeyCode() == keyLeft) {
+      if (!map.getPlayer().collisionLeft(map.getAllObjects())) {
+        map.getPlayer().walkLeft = true;
+      }
+    } else if (e.getKeyCode() == keyJump) {
+      if (map.getPlayer().collisionBottom(map.getAllObjects())) {
+        map.getPlayer().jump = true;
+      }
+    } else if (e.getKeyCode() == keyConsole) {
+      if (console == null) {
+        console = new Console(this);
+      } else {
+        console.setVisible(!console.isVisible());
+      }
+    } else if (e.getKeyCode() == 27) {// esc
+      pause();
     }
+  }
 
-    public void stoppause(Map map) {
-        map.setVisible(true);
-        gameloop.go();
-        revalidate();
-        repaint();
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if (e.getKeyCode() == keyRight) {
+      map.getPlayer().walkRight = false;
+    } else if (e.getKeyCode() == keyLeft) {
+      map.getPlayer().walkLeft = false;
+    } else if (e.getKeyCode() == keyJump) {
+      map.getPlayer().jump = false;
     }
+  }
 
-    public void setEndscreen() {
-        endscreen = new Endscreen(this, getcurrentmin(), getcurrents(), getcurrentms());
-        gameloop.pause();
-        remove(map);
-        this.add(endscreen, BorderLayout.CENTER);
-        endscreen.setVisible(true);
-        revalidate();
-        repaint();
-    }
+  public double getUptime() {
+    return upTime;
+  }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // wird immer wieder vom Timer aufgerufen, ist quasi die Gameloop
-        if (e.getSource() == timer) {
-        } else if (e.getSource() == timerm) {
-            // System.out.println("calls: " + calls);
-            // System.out.println("updates: " + updates);
-            // System.out.println("draws: " + draws);
-            // System.out.println("update Time: " + (afterUpdate - start));
-            // System.out.println("drawTime: " + (fertig - afterUpdate));
-            calls = 0;
-            updates = 0;
-            draws = 0;
-        } else if (e.getSource() == imageSwitcher) {
-            // switch image methode von jeder Mapobjekt klasse aufrufen
-            Water.switchImage();
-            Goal.switchImages();
-        }
-    }
+  public Map getMap() {
+    return map;
+  }
 
-    public void tick() {
-        calls++;
-        upTime += delayBetweenFrames;
-        start = System.currentTimeMillis();
-        map.update();
-        afterUpdate = System.currentTimeMillis();
-        map.draw();
-        fertig = System.currentTimeMillis();
-    }
+  public double getDelaybetweenFrames() {
+    return delayBetweenFrames;
+  }
 
-    public long getcurrents() {
-        return timerformap.calculateCurrentTimeInS();
-    }
+  public int getjumpkey() {
+    return keyJump;
+  }
 
-    public long getcurrentms() {
-        return timerformap.calculateCurrentTimeInMs();
-    }
+  public int getleftkey() {
+    return keyLeft;
+  }
 
-    public long getcurrentmin() {
-        return timerformap.calculateCurrentTimeInMin();
-    }
+  public int getrightkey() {
+    return keyRight;
+  }
 
-    public boolean alreadybound(int key) {
-        if (key == keyJump) {
-            return true;
-        } else if (key == keyRight) {
-            return true;
-        } else if (key == keyLeft) {
-            return true;
-        } else if (key == keyConsole) {
-            return true;
-        }
-        return false;
-    }
+  public int getconsolekey() {
+    return keyConsole;
+  }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-    }
+  public void setjumpkey(int key) {
+    this.keyJump = key;
+  }
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == keyRight) {
-            if (!map.getPlayer().collisionRight(map.getAllObjects())) {
-                map.getPlayer().walkRight = true;
-            }
-        } else if (e.getKeyCode() == keyLeft) {
-            if (!map.getPlayer().collisionLeft(map.getAllObjects())) {
-                map.getPlayer().walkLeft = true;
-            }
-        } else if (e.getKeyCode() == keyJump) {
-            if (map.getPlayer().collisionBottom(map.getAllObjects())) {
-                map.getPlayer().jump = true;
-            }
-        } else if (e.getKeyCode() == keyConsole) {
-            if (console == null) {
-                console = new Console(this);
-            } else {
-                console.setVisible(!console.isVisible());
-            }
-        } else if (e.getKeyCode() == 27) {// esc
-            pause();
-        }
-    }
+  public void setleftkey(int key) {
+    this.keyLeft = key;
+  }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == keyRight) {
-            map.getPlayer().walkRight = false;
-        } else if (e.getKeyCode() == keyLeft) {
-            map.getPlayer().walkLeft = false;
-        } else if (e.getKeyCode() == keyJump) {
-            map.getPlayer().jump = false;
-        }
-    }
+  public void setrightkey(int key) {
+    this.keyRight = key;
+  }
 
-    public double getUptime() {
-        return upTime;
-    }
+  public void setconsolekey(int key) {
+    this.keyConsole = key;
+  }
 
-    public Map getMap() {
-        return map;
-    }
+  public void setpaused(boolean b) {
+    paused = b;
+  }
 
-    public double getDelaybetweenFrames() {
-        return delayBetweenFrames;
-    }
-
-    public int getjumpkey() {
-        return keyJump;
-    }
-
-    public int getleftkey() {
-        return keyLeft;
-    }
-
-    public int getrightkey() {
-        return keyRight;
-    }
-
-    public int getconsolekey() {
-        return keyConsole;
-    }
-
-    public void setjumpkey(int key) {
-        this.keyJump = key;
-    }
-
-    public void setleftkey(int key) {
-        this.keyLeft = key;
-    }
-
-    public void setrightkey(int key) {
-        this.keyRight = key;
-    }
-
-    public void setconsolekey(int key) {
-        this.keyConsole = key;
-    }
-
-    public void setpaused(boolean b) {
-        paused = b;
-    }
-
-    public static void main(String[] args) {
-        new Game();
-    }
+  public static void main(String[] args) {
+    new Game();
+  }
 }
--- src/main/java/de/junkerjoerg12/scenes/Endscreen.java
+++ src/main/java/de/junkerjoerg12/scenes/Endscreen.java
@@ -21,74 +21,75 @@ import de.junkerjoerg12.tools.Statreader;
 import de.junkerjoerg12.tools.Statwriter;
 
 public class Endscreen extends JPanel implements ActionListener {
-    private Game game;
-    private Font timerFont = new Font("TimesRoman", Font.PLAIN, 20);
-    private JButton back = new JButton("back");
-    private String time;
+  private Game game;
+  private Font timerFont = new Font("TimesRoman", Font.PLAIN, 20);
+  private JButton back = new JButton("back");
+  private String time;
 
-    private long min;
-    private long sec;
-    private long ms;
+  private long min;
+  private long sec;
+  private long ms;
 
-    private BufferedImage background;
+  private BufferedImage background;
 
-    public Endscreen(Game game, Long min, long seconds, Long ms) {
-        this.game = game;
-        this.min = min;
-        this.sec = seconds;
-        this.ms = ms;
-        this.setLayout(new GridBagLayout());
-        back.addActionListener(this);
+  public Endscreen(Game game, Long min, long seconds, Long ms) {
+    this.game = game;
+    this.min = min;
+    this.sec = seconds;
+    this.ms = ms;
+    this.setLayout(new GridBagLayout());
+    back.addActionListener(this);
 
-        try {
-            background = ImageIO.read(new File(Paths.get("src", "main", "resources", "map1.png").toString()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        this.add(back, constraints);
-
-        time = "Your Time: " + min + ":" + seconds + "," + ms;
-        repaint();
-
-        compareToBest();
+    try {
+      background = ImageIO.read(new File(Paths.get("src", "main", "resources", "map1.png").toString()));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    private void compareToBest() {
-        ArrayList<String> best = new Statreader().getbest();
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.gridx = 1;
+    constraints.gridy = 1;
+    this.add(back, constraints);
 
-        long bestMin = Long.parseLong(best.get(0));
-        long bestSec = Long.parseLong(best.get(1));
-        long bestMs = Long.parseLong(best.get(2));
+    time = "Your Time: " + min + ":" + seconds + "," + ms;
+    repaint();
 
-        if (min < bestMin) {
-            new Statwriter(min + "\n" + sec + "\n" + ms);
-        } else if (min == bestMin && sec < bestSec) {
-            new Statwriter(min + "\n" + sec + "\n" + ms);
-        }
-        else if (min == bestMin & sec == bestSec && ms < bestMs) {
-            new Statwriter(min + "\n" + sec + "\n" + ms);
-        }
+    compareToBest();
+  }
 
+  private void compareToBest() {
+    ArrayList<String> best = new Statreader().getbest();
+
+    long bestMin = Long.parseLong(best.get(0));
+    long bestSec = Long.parseLong(best.get(1));
+    long bestMs = Long.parseLong(best.get(2));
+
+
+    if (bestMin == 0 && bestSec == 0 && bestMs == 0) {// if all 0 no highscore was set yet
+      new Statwriter(min + "\n" + sec + "\n" + ms);
+    } else if (min < bestMin) {
+      new Statwriter(min + "\n" + sec + "\n" + ms);
+    } else if (min == bestMin && sec < bestSec) {
+      new Statwriter(min + "\n" + sec + "\n" + ms);
+    } else if (min == bestMin & sec == bestSec && ms < bestMs) {
+      new Statwriter(min + "\n" + sec + "\n" + ms);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == back) {
-            game.remove(this);
-            game.levelauswahl();
-        }
-    }
+  }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(background, 0, 0, null);
-        g.setFont(timerFont);
-        g.drawString(time, 885, 600);
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == back) {
+      game.remove(this);
+      game.levelauswahl();
     }
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.drawImage(background, 0, 0, null);
+    g.setFont(timerFont);
+    g.drawString(time, 885, 600);
+  }
 }
