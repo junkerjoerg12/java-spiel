d33d6079dd4097ae14ba2bf39b9512b2e800259c
rcustic <jakob.meyer06@gmx.de>
2024-12-05 09:08:24 +0100

    Trap geadded(wie lava)

---
+++ src/main/java/de/junkerjoerg12/map/mapElements/Trap.java
@@ -0,0 +1,35 @@
+package de.junkerjoerg12.map.mapElements;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
+import de.junkerjoerg12.Game;
+import de.junkerjoerg12.map.MapElement;
+
+public class Trap extends MapElement{
+
+    public static BufferedImage imageToDisplay;
+
+    public Trap(Game game) {
+        super(game);
+        if (Stone.imageToDisplay == null) {
+            try {
+                Stone.imageToDisplay = ImageIO
+                            .read(new File(Paths.get("src", "main", "resources", "assets", "stone.png").toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        super.draw(g, this.getClass());
+    }
+
+}
